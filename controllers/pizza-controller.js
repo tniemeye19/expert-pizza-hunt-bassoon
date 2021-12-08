6b9256eda4bd7785599a444d4e5b118027dd99aa
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-12-08 17:14:15 -0500

    created validation for update, add functionality

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -51,7 +51,7 @@ const pizzaController = {
     // update pizza by id
     updatePizza({ params, body }, res) {
         // have to set new: true, otherwise it will return the original doucment
-        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
             .then(dbPizzaData => {
                 if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id!' });
